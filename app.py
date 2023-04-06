9a55252152b256869aa7f2c641fcc4f66695a4ce
TAMADA <a388087@gmail.com>
2023-04-06 20:10:13 +0800

    1st

--- app.py
+++ app.py
@@ -5,31 +5,46 @@
 # @Site    : 
 # @File    : app.py
 # @Software: streamlitTools
-
 import streamlit as st
 
-# 页面标题
-st.title("文件转换应用")
+# 定义转换函数
+def transform_text(text, transformation):
+    if transformation == "大写":
+        return text.upper()
+    elif transformation == "小写":
+        return text.lower()
+    elif transformation == "标题":
+        return text.title()
+    else:
+        return text
+
+# 设置页面标题
+st.title("文件操作")
 
 # 上传文件
-uploaded_file = st.file_uploader("请选择要上传的文件：", type=["txt",'pdf'])
+uploaded_file = st.file_uploader("选择一个文件")
 
-# 如果有文件上传
+# 设置转换选项
+transformation_options = ["原始", "大写", "小写", "标题"]
+selected_transformation = st.radio("选择转换方式", transformation_options)
+
+# 如果文件存在
 if uploaded_file is not None:
-    # 读取文件内容
-    file_contents = uploaded_file.getvalue().decode("utf-8")
+    # 将文件内容读入内存
+    file_contents = uploaded_file.read()
 
-    # 转换文件内容为大写字母
-    transformed_contents = file_contents.upper()
+    # 对文件内容进行操作
+    processed_text = transform_text(file_contents, selected_transformation)
 
-    # # 显示转换后的文件内容
-    # st.write("转换后的文件内容：")
-    # st.write(transformed_contents)
+    # 显示操作后的文件内容
+    st.write("处理后的文件内容：")
+    st.write(processed_text)
 
-    # 提供下载链接
+    # 提供下载操作后的文件
     st.download_button(
-        label="点击下载转换后的文件",
-        data=transformed_contents.encode("utf-8"),
-        file_name="converted_file.txt",
+        label="下载文件",
+        data=processed_text,
+        file_name="processed_file.txt",
         mime="text/plain"
     )
+
