0e1139957f78a5a8ae963dbf2bb8546a8853f07f
TAMADA <a388087@gmail.com>
2023-04-17 16:00:08 +0800

    增加多页

---
+++ pages/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 2023/4/17 15:59
+# @Author  : notI
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: streamlitTools
---
+++ pages/page3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 2023/4/17 15:59
+# @Author  : notI
+# @Site    : 
+# @File    : page3.py
+# @Software: streamlitTools
