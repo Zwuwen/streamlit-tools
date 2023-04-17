9f4fd19d3156fea59d292adeb241261f6c9392f3
TAMADA <a388087@gmail.com>
2023-04-17 16:34:13 +0800

    增加多页

---
+++ pages/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 2023/4/17 16:32
+# @Author  : notI
+# @Site    : 
+# @File    : __init__.py.py
+# @Software: streamlitTools
---
+++ pages/excel_to_xml.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 2023/4/17 16:33
+# @Author  : notI
+# @Site    : 
+# @File    : excel_to_xml.py
+# @Software: streamlitTools
