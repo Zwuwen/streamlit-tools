e88be8a440d7a8ac38240b5d526a2557d5b2271a
zhuxiufeng <a388087@gmail.com>
2023-06-30 13:52:00 +0800

    free+buff/cache

---
+++ scripts/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# 重启streamlit
+cd /home/zxf/workstation/web/github/streamlit/streamlit-tools
+source env/bin/activate
+pid=$(ps -aux | grep streamlit | grep -v grep | awk '{print $2}')
+if pid; then
+    echo "streamlit is running, pid is ${pid}"
+    kill -9 "${pid}"
+else
+    echo "streamlit is not running"
+fi
+nohup streamlit run memory_show.py >/dev/null 2>&1 &
+echo "Restarted streamlit"
