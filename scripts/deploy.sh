166af5d916728e8bbb4e87ba603518ec756a5924
zhuxiufeng <a388087@gmail.com>
2023-06-30 15:20:47 +0800

    free+buff/cache

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ source env/bin/activate
 pid=$(ps -aux | grep streamlit | grep -v grep | awk '{print $2}')
 if [ -n "${pid}" ]; then
     echo "streamlit is running, pid is ${pid}"
-    kill -9 "${pid}"
+    kill  -9  ${pid}
 else
     echo "streamlit is not running"
 fi
